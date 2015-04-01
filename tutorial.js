2b652b460087471f74a499568b94909947581564
Victor Fernandes <victorfern91@gmail.com>
2015-04-01 21:57:14 +0100

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -1,6 +1,6 @@
 //get node module
 var b64 = require('base64');
 
-var encoded = b64.encode('Victor Fernandes is cool, and cool!');
+var encoded = b64.encode('This is Sparta! And is Encoded?');
 console.log('Message encoded in base64 :', encoded);
 console.log('Message in base64 decoded : '+b64.decode(encoded)+'.');
